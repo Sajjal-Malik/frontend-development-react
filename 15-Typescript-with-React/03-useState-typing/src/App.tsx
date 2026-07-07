449b4787236058a1ff080c4af2e49336fbb437de
Sajjal Malik <sajjalmalik.sm@gmail.com>
2026-07-07 09:38:53 +0500

    UseState Typing code pushed

---
+++ 15-Typescript-with-React/03-useState-typing/src/App.tsx
@@ -0,0 +1,9 @@
+import Counter from "./Counter"
+
+const App = () => {
+  return (
+    <div><Counter /></div>
+  )
+}
+
+export default App
---
+++ 15-Typescript-with-React/04-uS-typing-practice/src/App.tsx
@@ -0,0 +1,13 @@
+import TodoList from "./TodoList"
+// import UserProfile from "./UserProfile"
+
+const App = () => {
+  return (
+    <div>
+      {/* <UserProfile /> */}
+      <TodoList />
+    </div>
+  )
+}
+
+export default App
