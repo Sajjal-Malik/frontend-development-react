4911c4a45cb33ed02c2d27b74f671fe5b1414107
Sajjal Malik <sajjalmalik.sm@gmail.com>
2026-07-16 09:11:56 +0500

    TailwindCSS and daisyUI configured

--- 16-Typescript-Projects/1.simple-filtering/src/App.tsx
+++ 16-Typescript-Projects/1.simple-filtering/src/App.tsx
@@ -1,10 +1,11 @@
 
+
 const App = () => {
   return (
-    <div className="bg-amber-200">
-
+    <div className="flex h-screen flex-col items-center justify-center gap-4 bg-base-200">
+      <h1 className="text-3xl font-bold text-primary">Tailwind & daisyUI Test</h1>
+      <button className="btn btn-secondary shadow-lg">If I am purple/pink, daisyUI works!</button>
     </div>
-  )
-}
-
-export default App
+  );
+};
+export default App;
