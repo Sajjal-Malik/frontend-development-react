ced94db9eccfa06f88ae66b4fb4c88dc4465f71d
Sajjal Malik <sajjalmalik.sm@gmail.com>
2026-07-21 09:54:59 +0500

    Basic Filtering TS Project Complete

--- 16-Typescript-Projects/1.simple-filtering/src/App.tsx
+++ 16-Typescript-Projects/1.simple-filtering/src/App.tsx
@@ -1,11 +1,11 @@
-
+import Dashboard from "./components/Dashboard";
 
 const App = () => {
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-4 bg-base-200">
-      <h1 className="text-3xl font-bold text-primary">Tailwind & daisyUI Test</h1>
-      <button className="btn btn-secondary shadow-lg">If I am purple/pink, daisyUI works!</button>
+    <div>
+      <Dashboard />
     </div>
   );
 };
+
 export default App;
