c7f093b270460c68e9590cdefd07dcd775db0233
Sajjal Malik <sajjalmalik.sm@gmail.com>
2026-08-13 15:02:05 +0500

    Transitions using Motion started

---
+++ 18-Framer-Motion/1.Motion-Basics/2.Transitions/src/App.tsx
@@ -0,0 +1,18 @@
+// import FadeComponent from "./components/1.FadeComponent";
+// import Sidebar from "./components/2.Sidebar";
+// import Modal from "./components/3.Modal";
+
+import ResponsiveButton from "./components/4.ResponsiveButton";
+
+const App = () => {
+  return (
+    <div>
+      {/*<FadeComponent />*/}
+      {/*<Sidebar />*/}
+      {/*<Modal />*/}
+      <ResponsiveButton />
+    </div>
+  )
+}
+
+export default App;
