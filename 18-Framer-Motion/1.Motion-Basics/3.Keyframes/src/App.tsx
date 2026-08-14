622ae44379098b339433d43234720f5e02a15c2b
Sajjal Malik <sajjalmalik.sm@gmail.com>
2026-08-14 11:18:55 +0500

    Keyframes using Motion Complete

---
+++ 18-Framer-Motion/1.Motion-Basics/3.Keyframes/src/App.tsx
@@ -0,0 +1,25 @@
+// import BouncingBall from "./components/1.BouncingBall";
+// import PulsatingButton from "./components/2.PulsatingButton";
+// import ColorChangeSquare from "./components/3.ColorChangeSquare";
+// import SlidingText from "./components/4.SlidingText";
+// import ZigzagAnimation from "./components/5.ZigZagAnimation";
+// import WaveEffect from "./components/6.WaveEffect";
+
+import AnimatedBackground from "./components/7.AnimatedBackground";
+
+
+const App = () => {
+  return (
+    <div>
+      {/*<BouncingBall />*/}
+      {/*<PulsatingButton />*/}
+      {/*<ColorChangeSquare />*/}
+      {/*<SlidingText />*/}
+      {/*<ZigzagAnimation />*/}
+      {/*<WaveEffect />*/}
+      <AnimatedBackground />
+    </div>
+  )
+}
+
+export default App;
