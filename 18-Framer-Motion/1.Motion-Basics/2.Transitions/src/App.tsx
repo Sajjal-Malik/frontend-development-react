7b7a54c2dc863cac40b480d761f4780212d885d6
Sajjal Malik <sajjalmalik.sm@gmail.com>
2026-08-14 10:36:30 +0500

    Transitions using Motion Complete

--- 18-Framer-Motion/1.Motion-Basics/2.Transitions/src/App.tsx
+++ 18-Framer-Motion/1.Motion-Basics/2.Transitions/src/App.tsx
@@ -1,8 +1,9 @@
 // import FadeComponent from "./components/1.FadeComponent";
 // import Sidebar from "./components/2.Sidebar";
 // import Modal from "./components/3.Modal";
-
-import ResponsiveButton from "./components/4.ResponsiveButton";
+// import ResponsiveButton from "./components/4.ResponsiveButton";
+// import Accordion from "./components/5.Accordion";
+import ToastNotification from "./components/6.ToastNotification";
 
 const App = () => {
   return (
@@ -10,7 +11,9 @@ const App = () => {
       {/*<FadeComponent />*/}
       {/*<Sidebar />*/}
       {/*<Modal />*/}
-      <ResponsiveButton />
+      {/*<ResponsiveButton />*/}
+      {/*<Accordion />*/}
+      <ToastNotification />
     </div>
   )
 }
