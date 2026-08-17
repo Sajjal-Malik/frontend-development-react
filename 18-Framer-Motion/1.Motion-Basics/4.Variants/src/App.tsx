29dab2ca29e259a5c24de4b5cb21747cbf58ea02
Sajjal Malik <sajjalmalik.sm@gmail.com>
2026-08-17 12:35:13 +0500

    Variants in Motion complete

--- 18-Framer-Motion/1.Motion-Basics/4.Variants/src/App.tsx
+++ 18-Framer-Motion/1.Motion-Basics/4.Variants/src/App.tsx
@@ -1,7 +1,20 @@
+// import FadeInComponent from "./components/1.FadeInComponent";
+// import SlidingMenu from "./components/2.SlidingMenu";
+// import Tooltip from "./components/3.Tooltip";
+// import ToggleSwitch from "./components/4.ToggleSwitch";
+
+import DynamicList from "./components/5.DynamicList";
+
 
 const App = () => {
   return (
-    <div></div>
+    <div>
+      {/*<FadeInComponent />*/}
+      {/*<SlidingMenu />*/}
+      {/*<Tooltip />*/}
+      {/*<ToggleSwitch />*/}
+      <DynamicList />
+    </div>
   )
 }
 
